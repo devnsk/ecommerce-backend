eb52df70c5b9f4196f494815412266c90d4847c3
devnsk <devnsk.2024@gmail.com>
2025-05-16 14:46:08 +0530

    @RepositoryRestResource annotation added

--- src/main/java/com/dev/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/dev/ecommerce/dao/ProductCategoryRepository.java
@@ -2,6 +2,8 @@ package com.dev.ecommerce.dao;
 
 import com.dev.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "productCategory",path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
