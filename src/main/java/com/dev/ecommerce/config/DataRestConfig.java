ff5966cf7e2c897bbfb5b1216e21b8dfa3bd4cc5
devnsk <devnsk.2024@gmail.com>
2025-05-16 14:44:57 +0530

    disabling the few methods

--- src/main/java/com/dev/ecommerce/config/DataRestConfig.java
+++ src/main/java/com/dev/ecommerce/config/DataRestConfig.java
@@ -1,4 +1,22 @@
 package com.dev.ecommerce.config;
 
-public class DataRestConfig {
+import com.dev.ecommerce.entity.Product;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import java.io.Serializable;
+
+public class DataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+//        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+        HttpMethod[] unsupportedMethods = {HttpMethod.GET, HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedMethods))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedMethods));
+    }
+
 }
